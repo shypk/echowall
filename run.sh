504fd290624f31f2cb3462f6d54514198f2a5462
devyco <seohyeokpark@naver.com>
2024-07-08 06:46:27 +0000

    init code

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+image_name="openresty/openresty:alpine"
+container_id=$(sudo docker ps | grep "$image_name" | awk '{print $1}')
+
+if [ -z "$container_id" ]; then
+  echo "No container found $image_name"
+else
+  echo "counter found: $container_id"
+  sudo docker stop $container_id
+fi
+
+
+sudo docker run -d -p 8080:8080 -v ./conf.d:/etc/nginx/conf.d -v ./echoroot/:/etc/nginx/echoroot/ $image_name
+
